224036ed7d321d6f5718cb9326898ff3abd45856
Yevhen Moshchonskyi <yevhen_moshchonskyi@external.mckinsey.com>
2019-02-19 15:24:22 +0200

    add readme add test scenarios

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# apt_inst_tests
+# apt-get install tests example
 
+
+
+Requirements
+------------
+
+- [Docker](https://docs.docker.com/install/)
+
+
+Notes
+-----
+
+Tests are executed inside docker container
+
+CI
+--
+
+CircleCI runs tests on branches 
+[![CircleCI](https://circleci.com/gh/moshchonskiy/apt_inst_tests.svg?style=svg)](https://circleci.com/gh/moshchonskiy/apt_inst_tests)
+
+
+Tests
+-----
+To run tests locally:
+
+- Clone repository
+
+- Build docker ./build_docker.sh
+
+- Tests can be executed by ./test.sh
--- tests/test_apt_install.py
+++ tests/test_apt_install.py
@@ -6,6 +6,15 @@ class TestAptGetInstall:
 
     @pytest.mark.positive
     def test_apt_get_install_success(self, shell_executor, remove_rolldice, host):
+        """Check apt-get install successfully installs a package
+
+           Pre-conditions
+               1. Fully uninstall a package
+
+           Scenario:
+               1. Install a package using apt-get install
+               2. Check if it was installed
+        """
         pkg_name = "rolldice"
         shell_executor.apt_install_pkg(pkg_name)
         pkg = host.package(pkg_name)
@@ -13,6 +22,15 @@ class TestAptGetInstall:
 
     @pytest.mark.positive
     def test_apt_get_install_version(self, shell_executor, remove_rolldice, host):
+        """Check apt-get install successfully installs a package with specified version
+
+           Pre-conditions
+               1. Fully uninstall a package
+
+           Scenario:
+               1. Install a specified version of a package using apt-get install
+               2. Check if it was installed
+        """
         version = "1.14-2"
         pkg_name = "rolldice"
         pkg_version = "{}={}".format(pkg_name, version)
@@ -23,6 +41,16 @@ class TestAptGetInstall:
 
     @pytest.mark.negative
     def test_apt_get_install_wrong_pkg_name(self, shell_executor, remove_rolldice, host):
+        """Check apt-get install can not install a package with wrong package name
+
+           Pre-conditions
+               1. Fully uninstall a package
+
+           Scenario:
+               1. Try to install a package using apt-get install
+               2. Check that apt-get returns an error
+               3. Check if it was not installed
+        """
         pkg_name = "rolldice"
         _, error = shell_executor.apt_install_pkg(pkg_name[:7])
         assert error, 'should return an error: Unable to locate package {}'.format(pkg_name[:7])
@@ -31,8 +59,19 @@ class TestAptGetInstall:
 
     @pytest.mark.negative
     def test_apt_get_install_wrong_version(self, shell_executor, remove_rolldice, host):
+        """Check apt-get install can not install a package with wrong package version
+               Pre-conditions
+                   1. Fully uninstall a package
+
+               Scenario:
+                   1. Try to install a package using apt-get install
+                   2. Check that apt-get returns an error
+                   3. Check if it was not installed
+        """
         version = "1.16-2"
         pkg_name = "rolldice"
         pkg_version = "{}={}".format(pkg_name, version)
         _, error = shell_executor.apt_install_pkg(pkg_version)
         assert error, "should return an error: Version '{}' for '{}' was not found".format(version, pkg_name)
+        pkg = host.package(pkg_name)
+        assert not pkg.is_installed, 'the specified pkg is unexpectedly installed: {}'.format(pkg_name)
